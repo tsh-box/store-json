a508460f6510599b044214c3f8c14a7d0b16db29
ToshBrown <Toshbrown@gmail.com>
2017-07-13 10:19:31 +0100

    Fix tests?

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ const SubscriptionManager = require('./lib/subscription/subscriptionManager.js')
 const timeseries = require('./timeseries.js');
 const keyvalue   = require('./keyvalue.js');
 
-const DATABOX_LOCAL_NAME = process.env.DATABOX_LOCAL_NAME || "store-blob";
+const DATABOX_LOCAL_NAME = process.env.DATABOX_LOCAL_NAME || "store-json";
 const DATABOX_LOCAL_PORT = process.env.DATABOX_LOCAL_PORT || 8080;
 const DATABOX_ARBITER_ENDPOINT = process.env.DATABOX_ARBITER_ENDPOINT || "https://arbiter:8080";
 
